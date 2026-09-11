dc515e9e51316d12979b0245f5bfd0db2f31b4a3
DenisEd9403 <denis.ern94@gmail.com>
2026-09-11 19:49:47 -0400

    Create const.py

---
+++ custom_components/valueclouds/const.py
@@ -0,0 +1,23 @@
+DOMAIN = "valueclouds"
+
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_DEVICE_PN = "device_pn"
+CONF_DEVICE_SN = "device_sn"
+CONF_DEV_CODE = "dev_code"
+CONF_DEV_ADDR = "dev_addr"
+CONF_SIGN = "sign"
+
+DEFAULT_DEV_CODE = "6422"
+DEFAULT_DEV_ADDR = "4"
+DEFAULT_I18N = "es_ES"
+DEFAULT_PROJECT = "IOT"
+DEFAULT_VW = "device"
+
+API_BASE = "https://api.valueclouds.com"
+
+LOGIN_ENDPOINT = "/ppr/web/login/login"
+LAST_DATA_ENDPOINT = "/ppe/api/auth/web/querySPDeviceLastData"
+ENERGY_FLOW_ENDPOINT = "/ppe/api/auth/web/deviceenergyflow"
+
+DEFAULT_SCAN_INTERVAL = 60
