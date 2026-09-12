afb706278b69349cadf093bb7504e4518ca2bc76
DenisEd9403 <denis.ern94@gmail.com>
2026-09-11 21:06:29 -0400

    const.py

--- custom_components/valueclouds/const.py
+++ custom_components/valueclouds/const.py
@@ -1,12 +1,11 @@
 DOMAIN = "valueclouds"
 
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
+CONF_TOKEN = "token"
+CONF_AUTH = "auth"
+CONF_SIGN = "sign"
+
 CONF_DEVICE_PN = "device_pn"
 CONF_DEVICE_SN = "device_sn"
-CONF_DEV_CODE = "dev_code"
-CONF_DEV_ADDR = "dev_addr"
-CONF_SIGN = "sign"
 
 DEFAULT_DEV_CODE = "6422"
 DEFAULT_DEV_ADDR = "4"
@@ -16,7 +15,6 @@ DEFAULT_VW = "device"
 
 API_BASE = "https://api.valueclouds.com"
 
-LOGIN_ENDPOINT = "/ppr/web/login/login"
 LAST_DATA_ENDPOINT = "/ppe/api/auth/web/querySPDeviceLastData"
 
 DEFAULT_SCAN_INTERVAL = 60
